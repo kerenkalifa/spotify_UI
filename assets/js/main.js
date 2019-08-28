face5ce5dd7c5098bac5f886672738d51a59e49a
Kerenbot <kerenkalifa@gmail.com>
2019-08-28 17:46:05 +0300

    modify index.html, style.css and main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
-function searchItem()
-{
-   inputText=$('#message').val();
-   alert(inputText);
-    $('#anotherMessage').val(inputText);
-
+function searchItem() {
+  inputText = $("#message").val();
+  alert(inputText);
+  $("#anotherMessage").val(inputText);
 }
 
+function showColor() {
+  var selectedColor=$("#color option:selected").text();
+  alert($("#color option:selected").text());
+}
