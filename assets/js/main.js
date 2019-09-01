c33cbd4881a25016ce83c819dfc51b7255c687ff
Kerenbot <kerenkalifa@gmail.com>
2019-09-01 12:30:26 +0300

    modified index.html, style.css and main.js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 function searchItem() {
   inputText = $("#message").val();
   alert(inputText);
-  $("#anotherMessage").val(inputText);
+  // $("#anotherMessage").val(inputText);
 }
 
-function showColor() {
-  var selectedColor = $("#color option:selected").text();
-  alert($("#color option:selected").text());
-}
+// function showColor() {
+//   var selectedColor = $("#color option:selected").text();
+//   alert($("#color option:selected").text());
+// }
 
 // function to change the selected color
-function changeColor() {
-  var selColor = document.getElementById("selColor");
-  var color = selColor.value;
-  rect.style.backgroundColor = color;
-}
+// function changeColor() {
+//   var selColor = document.getElementById("selColor");
+//   var color = selColor.value;
+//   rect.style.backgroundColor = color;
+// }
