ab4a95b5347d6aa160f2d93295b0e57df75ea88b
Kerenbot <kerenkalifa@gmail.com>
2019-09-08 20:33:09 +0300

    add table in index.html, add api request in main js, modify style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 function searchItem() {
   inputText = $("#message").val();
-  alert(inputText);
-  // $("#anotherMessage").val(inputText);
+  // alert(inputText);
+  $("#anotherMessage").val(inputText);
 }
 
 // function showColor() {
@@ -15,3 +15,43 @@ function searchItem() {
 //   var color = selColor.value;
 //   rect.style.backgroundColor = color;
 // }
+
+// var request = require('request');
+
+// request('https://reststop.randomhouse.com/resources/works/19312', function(error, response, body){
+//   if(!error && response.statusCode == 200){
+//     console.log(body);
+    
+  
+//   }
+// })
+
+
+
+// Send request
+// request.send()
+
+// $.ajax({
+//   url: "https://reststop.randomhouse.com/resources/works/19312",
+//   type: "GET",                    
+//   success: function(server_response) {      
+   
+//   }
+// });
+
+$.ajax({
+  type: "GET",
+  url: "https://reststop.randomhouse.com/resources/works/19312",
+  dataType: "xml",
+  success: function(xml) {
+    $(xml).find("result").each(function () {
+      $("table tbody").append("<tr>");
+      $("table tbody").append("<td>" + $(this).find("Authorweb").text() + "</td>");
+      $("table tbody").append("<td>" + $(this).find("Onsaledate").text() + "</td>");
+      $("table tbody").append("<td>" + $(this).find("Workid").text() + "</td>");
+      $("table tbody").append("<td>" + $(this).find("Titleshort").text() + "</td>");
+      $("table tbody").append("</tr>"); 
+      console.log('result');
+    });
+  }
+});
